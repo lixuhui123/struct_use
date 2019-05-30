c289d17727ff81fc76c6386c1c8915bcedb5d0c9
lixuhui123 <2871726754@qq.com>
2019-05-30 23:11:57 +0800

    结构体使用电话黄页

---
+++ 2019_5_29_作业/MailList.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef _INC_STDIO // include guard for 3rd party interop
+#define _INC_STDIO
+
+#endif // _INC_STDIO
---
+++ 2019_5_29_作业/源2.cpp
@@ -0,0 +1,38 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	struct S1
+//	{
+//		char c1;
+//		int i;
+//		char c2;
+//	};//12
+//	printf("%d\n", sizeof(struct S1));
+//	//练习2
+//	struct S2
+//	{
+//		char c1;
+//		char c2;
+//		int i;
+//	};//8
+//	printf("%d\n", sizeof(struct S2));
+//	//练习3
+//	struct S3
+//	{
+//		double d;
+//		char c;
+//		int i;
+//	};//16
+//	printf("%d\n", sizeof(struct S3));
+//	//练习4-结构体嵌套问题
+//	struct S4
+//	{
+//		char c1;
+//		struct S3 s3;
+//		double d;
+//	};//32
+//	printf("%d\n", sizeof(struct S4));
+//	system("pause");
+//	return 0;
+//}
